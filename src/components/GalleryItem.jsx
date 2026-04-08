61dd321816b71897f26f7484a529f0b55c224916
g-akca <gok.akca66@gmail.com>
2026-04-08 13:20:59 +0300

    Change GalleryItem structure

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -6,10 +6,7 @@ function GalleryItem({ image, title, artist, className }) {
         alt={`${title} painting by ${artist}`}
         className="w-full h-full object-cover"
       />
-
-      <div className="absolute bottom-0 w-full h-42.5 bg-linear-to-b from-transparent to-black/84" />
-
-      <div className="absolute bottom-8 left-8 text-white flex flex-col gap-2">
+      <div className="absolute bottom-0 w-full h-42.5 bg-linear-to-b from-transparent to-black/84 text-white p-8 flex flex-col justify-end gap-2">
         <p className="text-[24px] leading-[125%] font-bold">{title}</p>
         <p className="text-[13px] leading-[125%] opacity-75">{artist}</p>
       </div>
