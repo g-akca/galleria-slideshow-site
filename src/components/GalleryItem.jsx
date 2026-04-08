6ca53ea9b4d475c080afab9e0a06f6a620e0d4c6
g-akca <gok.akca66@gmail.com>
2026-04-08 13:13:05 +0300

    Add initial GalleryItem structure and styles

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,7 +1,21 @@
+import thumbnail from "/assets/starry-night/gallery.jpg";
+
 function GalleryItem() {
   return (
-    <>
-    </>
+    <div className="relative h-62.5">
+      <img
+        src={thumbnail}
+        alt="Starry Night painting by Vincent Van Gogh"
+        className="w-full h-full object-cover"
+      />
+
+      <div className="absolute bottom-0 w-full h-42.5 bg-linear-to-b from-transparent to-black/84" />
+
+      <div className="absolute bottom-8 left-8 text-white flex flex-col gap-2">
+        <p className="text-[24px] leading-[125%] font-bold">Starry Night</p>
+        <p className="text-[13px] leading-[125%] opacity-75">Vincent Van Gogh</p>
+      </div>
+    </div>
   )
 }
 
