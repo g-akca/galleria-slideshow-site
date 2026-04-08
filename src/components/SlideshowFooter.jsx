820f81bdaab72afc4a2657a4640f54d8d85d9dc5
g-akca <gok.akca66@gmail.com>
2026-04-08 20:39:15 +0300

    Add tablet responsive SlideshowFooter styles

--- src/components/SlideshowFooter.jsx
+++ src/components/SlideshowFooter.jsx
@@ -3,21 +3,21 @@ import nextImg from "/assets/shared/icon-next-button.svg";
 
 function SlideshowFooter({ currentIndex, setCurrentIndex, currentData, dataAmount }) {
   return (
-    <footer className="fixed bottom-0 w-screen z-50 bg-white py-4 px-6 flex justify-between items-center border-grey-150 border-t">
+    <footer className="fixed bottom-0 w-screen z-50 bg-white py-4 tablet:py-6 px-6 tablet:px-10 flex justify-between items-center border-grey-150 border-t">
       <div className="absolute -top-px left-0 h-px bg-black" style={{ width: `${((currentIndex + 1) / dataAmount) * 100}%` }} />
 
-      <div className="text-black">
-        <p className="font-bold tracking-base">{currentData.name}</p>
-        <p className="text-[10px] leading-[120%] opacity-75">{currentData.artist.name}</p>
+      <div className="text-black flex flex-col tablet:gap-2">
+        <p className="font-bold tracking-base tablet:tracking-0 tablet:text-[18px] tablet:leading-[125%]">{currentData.name}</p>
+        <p className="text-[10px] tablet:text-[13px] leading-[120%] tablet:leading-[125%] opacity-75">{currentData.artist.name}</p>
       </div>
       
-      <div className="flex gap-6">
+      <div className="flex gap-6 tablet:gap-10">
         <button type="button" onClick={() => setCurrentIndex(prev => prev - 1)} disabled={currentIndex === 0} className={`${currentIndex === 0 ? "opacity-15 cursor-not-allowed" : ""}`}>
-          <img src={backImg} alt="Back button" className="h-4" />
+          <img src={backImg} alt="Back button" className="h-4 tablet:h-6" />
         </button>
 
         <button type="button" onClick={() => setCurrentIndex(prev => prev + 1)} disabled={currentIndex === dataAmount - 1} className={`${currentIndex === dataAmount - 1 ? "opacity-15 cursor-not-allowed" : ""}`}>
-          <img src={nextImg} alt="Next button" className="h-4" />
+          <img src={nextImg} alt="Next button" className="h-4 tablet:h-6" />
         </button>
       </div>
     </footer>
