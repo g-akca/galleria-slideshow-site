3d37351206b7a4d33adbde45a8e772a8b6807aee
g-akca <gok.akca66@gmail.com>
2026-04-08 22:48:39 +0300

    Add tablet responsive SlideshowInfo styles

--- src/components/SlideshowInfo.jsx
+++ src/components/SlideshowInfo.jsx
@@ -1,7 +1,7 @@
 function SlideshowInfo({ currentData }) {
   return (
-    <div className="relative py-16 flex flex-col gap-16 -translate-y-4">
-      <p className="absolute top-0 right-0 -z-10 font-bold text-[100px] leading-[100%] text-grey-100">{currentData.year}</p>
+    <div className="relative py-16 tablet:py-18 tablet:px-29 flex flex-col gap-16 -translate-y-4">
+      <p className="absolute top-0 right-0 tablet:left-0 tablet:right-auto -z-10 font-bold text-[100px] tablet:text-[200px] leading-[100%] tablet:leading-[115%] tablet:tracking-[-0.2px] text-grey-100">{currentData.year}</p>
 
       <p className="font-bold tracking-base">{currentData.description}</p>
       <a href={currentData.source} className="font-bold text-[9px] leading-[125%] tracking-[2px] uppercase">Go to source</a>
