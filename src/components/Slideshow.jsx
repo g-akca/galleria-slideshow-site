02a27504053edaf19fef122501eaaf480348f00a
g-akca <gok.akca66@gmail.com>
2026-04-09 12:55:44 +0300

    Add desktop responsive SlideshowInfo styles

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -10,7 +10,7 @@ function Slideshow({ currentIndex, setCurrentIndex }) {
 
   return (
     <div className="grow flex flex-col pb-16 desktop:pb-48">
-      <div className="grow px-6 tablet:px-10 flex flex-col tablet:gap-16 desktop:flex-row desktop:justify-between">
+      <div className="grow px-6 tablet:px-10 flex flex-col tablet:gap-16 desktop:gap-8 desktop:flex-row desktop:justify-between">
         <SlideshowHeader currentData={currentData} />
         <SlideshowInfo currentData={currentData} />
       </div>
--- src/components/SlideshowInfo.jsx
+++ src/components/SlideshowInfo.jsx
@@ -1,6 +1,6 @@
 function SlideshowInfo({ currentData }) {
   return (
-    <div className="relative py-16 tablet:py-18 tablet:px-29 flex flex-col gap-16 -translate-y-4">
+    <div className="relative py-16 tablet:py-18 tablet:px-29 desktop:pl-0 desktop:pr-31.5 desktop:pt-27.5 desktop:pb-0 flex flex-col gap-16 desktop:gap-20 -translate-y-4 desktop:translate-y-0">
       <p className="absolute top-0 right-0 tablet:left-0 tablet:right-auto -z-10 font-bold text-[100px] tablet:text-[200px] leading-[100%] tablet:leading-[115%] tablet:tracking-[-0.2px] text-grey-100">{currentData.year}</p>
 
       <p className="font-bold tracking-base">{currentData.description}</p>
